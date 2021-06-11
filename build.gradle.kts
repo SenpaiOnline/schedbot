4e57b02fc8fdd44e61ec368776c2873a957728ed
Maxim Pavlov <maxpav137@gmail.com>
2021-06-11 18:24:11 +0500

    feat(1): add detekt
    Closes #1

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     kotlin("jvm") version "1.5.0"
     application
     id("me.qoomon.git-versioning") version "4.2.0"
+    id("io.gitlab.arturbosch.detekt").version("1.17.1")
 }
 
 repositories {
@@ -24,6 +25,9 @@ dependencies {
     implementation("io.honeybadger:honeybadger-java:2.0.2")
     implementation("com.github.ajalt.clikt:clikt:3.1.0")
     implementation("org.mapdb:mapdb:3.0.8")
+
+    detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.17.1")
+
     testImplementation("io.kotest:kotest-runner-junit5:4.4.3")
 }
 
@@ -48,23 +52,34 @@ group = "online.senpai"
 version = "0.0.0-SNAPSHOT"
 
 gitVersioning.apply(closureOf<me.qoomon.gradle.gitversioning.GitVersioningPluginConfig> {
-    branch(closureOf<me.qoomon.gradle.gitversioning.GitVersioningPluginConfig.VersionDescription>{
+    branch(closureOf<me.qoomon.gradle.gitversioning.GitVersioningPluginConfig.VersionDescription> {
         pattern = "main"
         versionFormat = "\${version}"
     })
-    branch(closureOf<me.qoomon.gradle.gitversioning.GitVersioningPluginConfig.VersionDescription>{
+    branch(closureOf<me.qoomon.gradle.gitversioning.GitVersioningPluginConfig.VersionDescription> {
         pattern = "feature/(?<feature>.+)"
         versionFormat = "\${feature}-SNAPSHOT"
     })
-    branch(closureOf<me.qoomon.gradle.gitversioning.GitVersioningPluginConfig.VersionDescription>{
+    branch(closureOf<me.qoomon.gradle.gitversioning.GitVersioningPluginConfig.VersionDescription> {
         pattern = "pull/(.+)"
         versionFormat = "\${branch}-SNAPSHOT"
     })
-    tag(closureOf<me.qoomon.gradle.gitversioning.GitVersioningPluginConfig.VersionDescription>{
+    tag(closureOf<me.qoomon.gradle.gitversioning.GitVersioningPluginConfig.VersionDescription> {
         pattern = "v(?<tagVersion>[0-9].*)"
         versionFormat = "\${tagVersion}"
     })
 })
+
+detekt {
+    config = files("detekt.yml")
+    buildUponDefaultConfig = true
+    reports {
+        html {
+            enabled = true
+            destination = file("reports/detekt.html")
+        }
+    }
+}
 
 application {
     mainClass.set("online.senpai.schedbot.MainKt")
