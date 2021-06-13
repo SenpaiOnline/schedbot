64c13153379243b7f0fd971fe9754beff36fc03f
Maxim Pavlov <maxpav137@gmail.com>
2021-06-13 17:24:19 +0500

    feat(2): extend the SlashCommand interface to add functionality

--- src/main/kotlin/online/senpai/schedbot/command/SlashCommand.kt
+++ src/main/kotlin/online/senpai/schedbot/command/SlashCommand.kt
@@ -1,7 +1,16 @@
 package online.senpai.schedbot.command
 
-import discord4j.discordjson.json.ImmutableApplicationCommandRequest
+import discord4j.core.event.domain.InteractionCreateEvent
+import discord4j.discordjson.json.ApplicationCommandRequest
+import reactor.core.publisher.Mono
 
 sealed interface SlashCommand {
-    val commandRequest: ImmutableApplicationCommandRequest
+    val commandRequest: ApplicationCommandRequest
+    val scope: Scope
+    fun handler(event: InteractionCreateEvent): Mono<Void>
+
+    sealed interface Scope {
+        data class Guild(val guildId: Long) : Scope
+        object Global : Scope
+    }
 }
