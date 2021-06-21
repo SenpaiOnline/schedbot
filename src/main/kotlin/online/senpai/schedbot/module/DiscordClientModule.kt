1c9d2569b207f105ef1518ca6767adcd3bb6a415
Maxim Pavlov <maxpav137@gmail.com>
2021-06-21 11:39:24 +0500

    feat(3): switch to Schedulers.immediate to minimize thread switching

--- src/main/kotlin/online/senpai/schedbot/module/DiscordClientModule.kt
+++ src/main/kotlin/online/senpai/schedbot/module/DiscordClientModule.kt
@@ -2,11 +2,13 @@ package online.senpai.schedbot.module
 
 import discord4j.core.DiscordClient
 import discord4j.core.GatewayDiscordClient
+import discord4j.core.event.EventDispatcher
 import discord4j.core.shard.MemberRequestFilter
 import discord4j.core.shard.ShardingStrategy
 import discord4j.gateway.intent.IntentSet
 import org.koin.core.module.Module
 import org.koin.dsl.module
+import reactor.core.scheduler.Schedulers
 
 val discordClientModule: (String, Long) -> Module = { token: String, intents: Long ->
     module {
@@ -18,6 +20,12 @@ val discordClientModule: (String, Long) -> Module = { token: String, intents: Lo
                     setEnabledIntents(IntentSet.of(intents))
                     setMemberRequestFilter(MemberRequestFilter.none())
                     setSharding(ShardingStrategy.single())
+                    setEventDispatcher(
+                        EventDispatcher
+                            .builder()
+                            .eventScheduler(Schedulers.immediate())
+                            .build()
+                    )
                 }
                 .login()
                 .block()
