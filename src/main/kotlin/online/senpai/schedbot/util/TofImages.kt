2ed7fad5d3942c65d08ded4ec6122ba037cf6995
Maxim Pavlov <maxpav137@gmail.com>
2021-08-27 18:50:53 +0500

    feat(6): add some constants for images to make messages look a bit fancy

---
+++ src/main/kotlin/online/senpai/schedbot/util/TofImages.kt
@@ -0,0 +1,8 @@
+package online.senpai.schedbot.util
+
+// TODO
+const val TOF_MYOW = "https://cdn.discordapp.com/attachments/803383152688365602/880449429663002634/tofmyow.png"
+const val TOF_HEH = "https://cdn.discordapp.com/attachments/803383152688365602/880449591634432081/tofheh.png"
+const val TOF_SLY = "https://cdn.discordapp.com/attachments/803383152688365602/879638990775529482/tofsly.png"
+const val TOF_NYAN = "https://cdn.discordapp.com/attachments/803383152688365602/880478523947634708/tofnyan.png"
+const val TOF_HMM = "https://cdn.discordapp.com/attachments/803383152688365602/880721637249527828/tofhmm.png"
